c33b8eaa14c4ac09caf0e0318538f779fc9b771b
webashrafians <webashrafians@gmail.com>
2020-10-13 01:21:03 +0600

    firebase deploy

---
+++ src/components/Login/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBtfkwI4imM3JXqHa5FkKzP14sZ5xemVJ4",
+  authDomain: "doctors-portal-appointment.firebaseapp.com",
+  databaseURL: "https://doctors-portal-appointment.firebaseio.com",
+  projectId: "doctors-portal-appointment",
+  storageBucket: "doctors-portal-appointment.appspot.com",
+  messagingSenderId: "155530979048",
+  appId: "1:155530979048:web:6183500e7e7e14a8198b5d"
+};
+export default firebaseConfig;
